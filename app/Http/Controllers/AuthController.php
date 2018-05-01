ce82beac19d02e58ad192f38e5da881eb2b1d42c
ghostd93 <p.kuba1993@gmail.com>
2018-05-01 10:45:48 +0200

    poprawki

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,6 +30,17 @@ class AuthController extends Controller
      */
     public function login()
     {
+        $validator = Validator::make(\request()->all(), [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'message' => $validator->errors()
+            ], 200);
+        }
+
         $credentials = request(['email', 'password']);
 
         $user = User::where('email', $credentials['email'])->first();
